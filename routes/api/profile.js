1e5e2c806e092828e9ca91d93de4449a58a47c3d
mohamed amine salah <salah.mohamedamine@fsm.u-monastir.tn>
2020-07-27 00:28:49 +0100

    add & delete profile education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -209,7 +209,6 @@ router.put('/experience', [auth, [
 // @desc  Delete experience from profile
 // @access Private
 
-
 router.delete('/experience/:exp_id', auth, async (req, res) => {
   try {
 
@@ -232,6 +231,89 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   }
 })
 
+
+/**/
+
+
+// @route Put api/profile/education
+// @desc  add profile education
+// @access Private
+
+
+router.put('/education', [auth, [
+  check('school', 'school is required').not().isEmpty(),
+  check('degree', 'degree is required').not().isEmpty(),
+  check('fieldofstudy', 'field of study is required').not().isEmpty(),
+  check('from', 'from date is required').not().isEmpty(),
+]], async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: errors.array() })
+  }
+
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  } = req.body;
+
+  const newEdu = {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  }
+
+  //deal with mongodb
+  try {
+    //fetch the profile which we gonna add to him the experience
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.education.unshift(newEdu);
+
+    await profile.save();
+
+    res.json(profile);
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
+})
+
+// @route Delete api/profile/education/:exp_id
+// @desc  Delete education from profile
+// @access Private
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //Get remove index
+    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile)
+
+    //res.send('experience deleted')
+  } catch (error) {
+    console.log(error.message);
+
+    res.status(500).send('Server Error');
+  }
+})
 
 
 
