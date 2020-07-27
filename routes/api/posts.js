9f10560c1c6160160cd9a2c74feba66bca89808d
mohamed amine salah <salah.mohamedamine@fsm.u-monastir.tn>
2020-07-27 04:28:25 +0100

    add & remove comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -31,17 +31,12 @@ router.post('/', [auth, [
       user: req.user.id
     })
     const post = await newPost.save();
-
     return res.status(200).json(post)
-
-
 
   } catch (error) {
     console.log(error);
     res.status(500).send('Server Error');
   }
-
-
 });
 
 
@@ -160,14 +155,98 @@ router.put('/unlike/:id', auth, async (req, res) => {
 
     const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
     post.likes.splice(removeIndex,1);
+    await post.save();
+    res.json(post.likes)
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Server Error');
+  }
+});
 
+
+// @route  POST api/posts/comment/:id
+// @desc   Comment on a post
+// @access Private
+
+router.post('/comment/:id', [auth, [
+  check('text', 'text is required').not().isEmpty(),
+]], async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+
+    post.comments.unshift(newComment);
 
     await post.save();
-
-    res.json(post.likes)
+    return res.status(200).json(post.comments)
 
   } catch (error) {
     console.log(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+// @route  Delete api/posts/comment/:id/:comment_id
+// @desc   Delete a comment
+// @access Private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    //pull put comment (get comment from the post)
+    //return comment if true else return false if comment dosen't exist
+    const comment = post.comments.find(comment => comment.id == req.params.comment_id);
+
+    //Make sure comment exist
+    if(!comment) {
+      return res.status(404).json({msg : 'Comment does not exist'});
+    }
+
+    //check user (check if the user how's delete the comment is the user how write the comment)
+    //console.log(comment.user);
+    if(comment.user.toString() !== req.user.id){
+      return res.status(401).json({msg :'user not authorized'});
+    }
+
+    //check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'user not authorized' });
+    }
+
+    //Get remove index
+
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments)
+
+  } catch (error) {
+    console.log(error);
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
     res.status(500).send('Server Error');
   }
 });
